365666ec9a6464fe64fcec92f09cce8da4a16fdc
GunnarHorve <gunnarhorve@Gunnar-Horves-MacBook-Pro.local>
2016-06-25 18:48:47 -0700

    Bound the data in Join Game

--- tab/Controllers/JoinGameController.swift
+++ tab/Controllers/JoinGameController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class JoinGameController: UIViewController {
     
+    @IBOutlet weak var gameId: UITextField!
+    @IBOutlet weak var playerId: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //fix keyboard bug
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(NewGameController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        //disable autocorrections
+        playerId.autocorrectionType = .No
+        gameId.autocorrectionType = .No
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func joinGamePressed(sender: AnyObject) {
+        print("Name Selection: \(self.playerId.text)")
+        print("Game Selection: \(self.gameId.text)")
+    }
 }
-
