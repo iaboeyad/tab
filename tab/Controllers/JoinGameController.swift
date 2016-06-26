d13369ee0d0b56c9d5c1ee9cc0e5ca8cd28fbcf6
GunnarHorve <gunnarhorve@Gunnar-Horves-MacBook-Pro.local>
2016-06-26 04:50:54 -0700

    Small usability fixes
    -Return now closes text field keyboards
    -Added emoji to Prof. Oak alert
    -cut silence from unsheathe.mp3 audio
    -made time spinbox collapsible based upon mode

--- tab/Controllers/JoinGameController.swift
+++ tab/Controllers/JoinGameController.swift
@@ -9,15 +9,16 @@
 import UIKit
 import FirebaseDatabase
 
-class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var gameId: UITextField!
     @IBOutlet weak var playerId: UITextField!
     @IBOutlet weak var picker: UIPickerView!
-    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var pickerLabel: UILabel!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var joinGameButton: UIButton!
     @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
     
     var pickerData: [Int] = [Int]()
     var timeSelection: Int = 1
@@ -36,17 +37,39 @@ class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerView
         playerId.autocorrectionType = .No
         gameId.autocorrectionType = .No
         
-        // Connect data:
-        populatePicker()
+        // Connect delegates:
+        self.gameId.delegate = self
+        self.playerId.delegate = self
+        self.picker.delegate = self
+        self.picker.dataSource = self
         
-        // Update view:
+        // Update UI objects:
+        populatePicker()
         toggleSelection()
+        
+        // Some bullshit magic??
+        makeMagicButton()
     }
     
+    func makeMagicButton() {
+        let pulseAnimation = CABasicAnimation(keyPath: "opacity")
+        pulseAnimation.duration = 2
+        pulseAnimation.fromValue = 0.3
+        pulseAnimation.toValue = 1
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = FLT_MAX
+        self.playButton.layer.addAnimation(pulseAnimation, forKey: nil)
+    }
     //Calls this function when the tap is recognized.
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
     
     override func didReceiveMemoryWarning() {
@@ -55,9 +78,8 @@ class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerView
     }
     
     func populatePicker() {
-        self.picker.delegate = self
-        self.picker.dataSource = self
-        for (var i = 1; i <= 60; i += 1) {
+        
+        for (var i = 1; i <= 6000; i += 1) {
             pickerData.append(i);
         }
     }
@@ -98,23 +120,27 @@ class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerView
         if(joinGameSelected) {
             joinGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 22)
             startGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 15)
+            picker.hidden = true
+            pickerLabel.hidden = true
         } else {
             joinGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 15)
             startGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 22)
+            picker.hidden = false
+            pickerLabel.hidden = false
         }
     }
     @IBAction func playPressed(sender: UIButton) {
         if (self.gameId.text!.isEmpty || self.playerId.text!.isEmpty) {
-            let alertController: UIAlertController = UIAlertController(title: "It's Unsafe!", message: "Wild Pokemon live in tall... err... please fill in your text fields", preferredStyle: UIAlertControllerStyle.Alert)
+            let alertController: UIAlertController = UIAlertController(title: "It's Unsafe!  😨", message: "Wild Pokemon live in tall... err... please fill in your text fields", preferredStyle: UIAlertControllerStyle.Alert)
             let okAction = UIAlertAction(title: "...", style: UIAlertActionStyle.Default, handler: nil)
             alertController.addAction(okAction)
             presentViewController(alertController, animated: true, completion: nil)
         } else {
-//            self.ref.child(self.gameId.text!).setValue(["endTime" : NSDate().timeIntervalSince1970+1000
-//                ,"flagHolder" :"current user"
-//                ,"gameName" : self.gameId.text!
-//                ,"numberOfPlayers":1
-//                ,"players":nil])
+            //            self.ref.child(self.gameId.text!).setValue(["endTime" : NSDate().timeIntervalSince1970+1000
+            //                ,"flagHolder" :"current user"
+            //                ,"gameName" : self.gameId.text!
+            //                ,"numberOfPlayers":1
+            //                ,"players":nil])
             performSegueWithIdentifier("playSegue", sender: nil)
         }
     }
