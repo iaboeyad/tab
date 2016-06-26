59d3ca5ea04207cbfee935d02b7ec4982c7b1eef
GunnarHorve <gunnarhorve@Gunnar-Horves-MacBook-Pro.local>
2016-06-26 02:32:13 -0700

    Merged "new" & "join" pages.
    Heavy graphical upgrade:
    -new background
    -new image resources
    -new layouts
    -new reactive selection buttons
    To come:
    -updating the button page

--- tab/Controllers/JoinGameController.swift
+++ tab/Controllers/JoinGameController.swift
@@ -8,22 +8,34 @@
 
 import UIKit
 
-class JoinGameController: UIViewController {
+class JoinGameController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var gameId: UITextField!
     @IBOutlet weak var playerId: UITextField!
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var joinGameButton: UIButton!
+    @IBOutlet weak var startGameButton: UIButton!
+    
+    var pickerData: [Int] = [Int]()
+    var timeSelection: Int = 1
+    var joinGameSelected = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //fix keyboard bug
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(NewGameController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(JoinGameController.dismissKeyboard))
         view.addGestureRecognizer(tap)
         
         //disable autocorrections
         playerId.autocorrectionType = .No
         gameId.autocorrectionType = .No
+        
+        // Connect data:
+        populatePicker()
     }
     
     //Calls this function when the tap is recognized.
@@ -36,20 +48,65 @@ class JoinGameController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func joinGamePressed(sender: AnyObject) {
-        if (self.gameId.text!.isEmpty || self.playerId.text!.isEmpty)
-        {
-            let alertController: UIAlertController = UIAlertController(title: "Woah There!", message: "Fill in some text!!", preferredStyle: UIAlertControllerStyle.Alert)
-            let okAction = UIAlertAction(title: "Ok...", style: UIAlertActionStyle.Default, handler: nil)
+    
+    func populatePicker() {
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        for (var i = 1; i <= 60; i += 1) {
+            pickerData.append(i);
+        }
+    }
+    
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerData[row % pickerData.count])
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        timeSelection = Int(pickerData[row])
+    }
+    
+    @IBAction func joinGamePressed(sender: UIButton) {
+        joinGameSelected = true
+        toggleSelection()
+    }
+    @IBAction func joinGame2Pressed(sender: UIButton) {
+        joinGamePressed(sender)
+    }
+    @IBAction func startGamePressed(sender: UIButton) {
+        joinGameSelected = false
+        toggleSelection()
+    }
+    @IBAction func startGame2Pressed(sender: UIButton) {
+        startGamePressed(sender)
+    }
+    
+    func toggleSelection() {
+        if(joinGameSelected) {
+            joinGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 22)
+            startGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 15)
+        } else {
+            joinGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 15)
+            startGameButton.titleLabel!.font =  UIFont(name: "Viking", size: 22)
+        }
+    }
+    @IBAction func playPressed(sender: UIButton) {
+        if (self.gameId.text!.isEmpty || self.playerId.text!.isEmpty) {
+            let alertController: UIAlertController = UIAlertController(title: "It's Unsafe!", message: "Wild Pokemon live in tall... err... please fill in your text fields", preferredStyle: UIAlertControllerStyle.Alert)
+            let okAction = UIAlertAction(title: "...", style: UIAlertActionStyle.Default, handler: nil)
             alertController.addAction(okAction)
             presentViewController(alertController, animated: true, completion: nil)
         } else {
-        performSegueWithIdentifier("joinToButton", sender: nil)
+            performSegueWithIdentifier("playSegue", sender: nil)
         }
-    }
-    @IBAction func startGamePressed(sender: AnyObject) {
-        performSegueWithIdentifier("joinToNew", sender: nil)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
