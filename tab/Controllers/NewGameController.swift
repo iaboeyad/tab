7faf1218d950c69a8afd223f4d8e3b34e7cb1173
GunnarHorve <gunnarhorve@Gunnar-Horves-MacBook-Pro.local>
2016-06-25 18:07:23 -0700

    Updated Storyboard
    removed initial entry slide.
    Make duration selector have values

--- tab/Controllers/NewGameController.swift
+++ tab/Controllers/NewGameController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
-class NewGameController: UIViewController {
+class NewGameController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var playerId: UITextField!
+    @IBOutlet weak var gameId: UITextField!
+    
+    var pickerData: [Int] = [Int]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        populatePicker()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func populatePicker() {
+        for (var i = 1; i <= 60; i += 1) {
+            pickerData.append(i);
+        }
+    }
+
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(pickerData[row % pickerData.count])
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(Int(pickerData[row]))
+    }
+    
+    
 }
 
